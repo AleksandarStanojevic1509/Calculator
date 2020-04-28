f0aea35a1305d92c96d8666a31194195452b7520
AleksandarStanojevic1509 <aleksandar.stanojevic.1509@gmail.com>
2020-04-29 01:02:49 +0200

    edit logic

--- app.js
+++ app.js
@@ -1,112 +1,114 @@
 //DOM
-let numberBtns = document.querySelectorAll('.number'); // selektuje sve brojeve 0 i .
-let mathOpBtns = document.querySelectorAll('.math-op'); // selektuje matematicke operacije
-let allClear = document.querySelector('#all-clear');
-let deleteBtn = document.querySelector('#delete');
-let equalBtn = document.querySelector('#equals');
-let currentOperandDisplay = document.querySelector('#disp-item-current');
-let previouseOperandDisplay = document.querySelector('#disp-item-previous');
+let numberBtns = document.querySelectorAll(".number");
+let mathOpBtns = document.querySelectorAll(".math-op");
+let allClear = document.querySelector("#all-clear");
+let deleteBtn = document.querySelector("#delete");
+let equalBtn = document.querySelector("#equals");
+let currentOperandDisplay = document.querySelector("#disp-item-current");
+let previouseOperandDisplay = document.querySelector("#disp-item-previous");
 
-//Varijable
-let currentOperand = '';
-let previouseOperand = '';
-let operator = '';
-let floatResult = '';
+let currentOperand = "";
+let previouseOperand = "";
+let operator = "";
 
-// Function
+const deleteLast = () => {
+  currentOperand = currentOperand.toString().slice(0, -1);
+};
 
-let calculate = (current, previouse, operator) => {
-  let currentOperand = Number(current)
-  let previouseOperand = Number (previouse) 
-  switch (operator) {
-    case '+':
-      return currentOperand + previouseOperand;
-    case '-':
-      return previouseOperand - currentOperand;
-    case '*':
-      return currentOperand * previouseOperand;
-    case '/':
-      if (currentOperand === 0) {
-        return 'Not allowed';
+const clearAll = () => {
+  currentOperand = "";
+  previouseOperand = "";
+  operator = "";
+};
+
+const calculate = (curr, prev, oper) => {
+  let previous = parseFloat(prev);
+  let current = parseFloat(curr);
+
+  if (isNaN(previous) || isNaN(current)) return;
+
+  switch (oper) {
+    case "+":
+      return previous + current;
+    case "-":
+      return previous - current;
+    case "*":
+      return previous * current;
+    case "/":
+      if (current === 0) {
+        return "Not allowed";
       } else {
-        return previouseOperand / currentOperand;
+        return previous / current;
       }
+    default:
+      return;
   }
 };
 
-// Listen
-
-numberBtns.forEach(n => {
-  n.addEventListener('click', () => {
-    if (n.id === '.' && currentOperand.includes('.')) return;
-    currentOperand += n.id;
-    currentOperandDisplay.innerHTML = currentOperand;
-  });
-});
-
-mathOpBtns.forEach(o => {
-  o.addEventListener('click', () => {
-    if(currentOperand.toString() == '') return
-    if (previouseOperand.toString() === ''){
-      previouseOperand = currentOperand;
-      currentOperand = '';
-      operator = o.id;
-      if (floatResult !== ''){
-        previouseOperandDisplay.innerHTML = floatResult + operator;
-      }
-      else{
-        previouseOperandDisplay.innerHTML = previouseOperand + operator;
-      }
-      currentOperandDisplay.innerHTML = '';
-      floatResult = ''
-    } 
-    // if (previouseOperand.toString() !== '' ){
-    //   operator = o.id
-    //   if( currentOperand.toString() !='' && previouseOperand.toString() != ''){
-    //     let calcResult = calculate(currentOperand, previouseOperand , operator);
-
-    //     //currentOperandDisplay.innerHTML = calcResult
-    //   }
-    //  // currentOperandDisplay.innerHTML = ''
-    //  // console.log (calcResult)
-    //   // currentOperand = '';
-    //   // currentOperandDisplay.innerHTML = ''
-
-    // }
-
-
-
-
-  
-  });
-});
-
-equalBtn.addEventListener('click', () => {
-  if (currentOperand === '' || previouseOperand === '') return;
-  let calcResult = calculate(currentOperand, previouseOperand , operator);
-  if (calcResult % 1 !== 0) {
-    floatResult = calcResult.toFixed(2);
-    currentOperandDisplay.innerHTML = floatResult
-  } else{
-
-    currentOperandDisplay.innerHTML = calcResult;
+const addOperation = (op) => {
+  if (!isNaN(previouseOperand) && currentOperand === "") {
+    operator = op;
   }
-  currentOperand = calcResult;
+  if (currentOperand === "") return;
+  if (previouseOperand !== "") {
+    currentOperand = calculate(currentOperand, previouseOperand, operator);
+  }
+  operator = op;
+  previouseOperand = currentOperand;
+  currentOperand = "";
+};
 
-  previouseOperandDisplay.innerHTML = '';
-  opereator = '';
+const appendNumber = (number) => {
+  if (number == "." && currentOperand.includes(".")) return;
+  return (currentOperand = currentOperand.toString() + number.toString());
+};
+
+const formatDisplay = (operand) => {
+  let operandNumber = parseFloat(operand);
+  if (isNaN(operandNumber)) return "";
+  return operandNumber.toLocaleString();
+};
+
+const updateDisplay = () => {
+  currentOperandDisplay.textContent = formatDisplay(currentOperand);
+  if (operator !== "") {
+    previouseOperandDisplay.textContent = `${formatDisplay(previouseOperand)} ${operator}`;
+  } else {
+    previouseOperandDisplay.textContent = formatDisplay(previouseOperand);
+  }
+};
+
+// Listener
+
+numberBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    appendNumber(btn.id);
+    updateDisplay();
+  });
 });
 
-allClear.addEventListener('click', () => {
-  currentOperandDisplay.innerHTML = '';
-  previouseOperandDisplay.innerHTML = '';
-  currentOperand = '';
-  previouseOperand = '';
-  operator = '';
+mathOpBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    addOperation(btn.id);
+    updateDisplay();
+  });
 });
 
-deleteBtn.addEventListener('click', () => {
-  console.log('ewe');
-  currentOperand = currentOperand.toString().slice(0, -1);
-  currentOperandDisplay.innerHTML = currentOperand;
+equalBtn.addEventListener("click", () => {
+  if (currentOperand === "" && previouseOperand === "") return;
+  if (currentOperand !== "" && previouseOperand === "") return;
+  currentOperand = calculate(currentOperand, previouseOperand, operator);
+  previouseOperand = "";
+  operator = "";
+  updateDisplay();
+});
+
+allClear.addEventListener("click", () => {
+  clearAll();
+  updateDisplay();
+});
+
+deleteBtn.addEventListener("click", () => {
+  deleteLast();
+  updateDisplay();
 });
